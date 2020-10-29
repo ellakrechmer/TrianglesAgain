b66079d91c6c90fc5f58f11f53403fb733fe14c2
ellakrechmer <ekrechmer20@stuy.edu>
2020-10-28 21:21:49 -0400

    added and tested equals method

--- Point.java
+++ Point.java
@@ -17,4 +17,7 @@ public class Point{
   public double distanceTo(Point other){
     return Math.sqrt((x-other.x)*(x-other.x)+(y-other.y)*(y-other.y));
   }
+  public boolean equals(Point other){
+    return (x==other.x && y==other.y);
+  }
 }
--- PointTester.java
+++ PointTester.java
@@ -1,7 +1,10 @@
 public class PointTester{
   public static void main(String[] args){
     Point A = new Point(3,5);
-    Point B = new Point(4,7);
-    System.out.println(A.distanceTo(B));
+    Point B = new Point(A);
+    Point C = new Point(4,7);
+    System.out.println(A.distanceTo(C));
+    System.out.println(A.equals(B));
+    System.out.println(A.equals(C));
   }
 }
